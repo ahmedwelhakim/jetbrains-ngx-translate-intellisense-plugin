61991a8c17ee9e61e670e16c16b99a128e09e35e
GitHub Action <action@github.com>
2025-10-30 10:49:19 +0000

    Template cleanup

--- src/main/kotlin/com/github/ahmedwelhakim/jetbrainngxtranslatetoolkitplugin/MyBundle.kt
+++ src/main/kotlin/com/github/ahmedwelhakim/jetbrainngxtranslatetoolkitplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.ahmedwelhakim.jetbrainngxtranslatetoolkitplugin
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
